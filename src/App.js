b44402544c32fe181c218b04fbce6db4f1e68f32
iqsyal21 <iqsyal21@gmail.com>
2022-12-18 14:30:26 +0700

    update fe-admin v1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Akun from './components/akun/Akun';
+import Verifikasi from './components/akun/Verifikasi';
+import Dashboard from './components/Dashboard/Dashboard';
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Akun} />
+        <Route path="/akun" component={Akun} />
+        <Route path="/verifikasi" component={Verifikasi} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
